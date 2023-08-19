53f58e6031404c7d5c3bc18182aae098023d8914
Lefteris Eleftheriades <lefteris.eleftheriades@gmail.com>
2023-08-19 18:57:45 +0300

    Changed touch move controls to joystick

--- touch.js
+++ touch.js
@@ -1,3 +1,64 @@
+class Joystick {
+    constructor() {
+        this.y1 = 400;
+        this.y2 = 546;
+        this.areas = [
+            {x1: 0,   x2: 75,  direction: Directions.Left, run: true},
+            {x1: 75,  x2: 150, direction: Directions.Left, run: false},
+            {x1: 150, x2: 225, direction: Directions.Idle, run: false},
+            {x1: 225, x2: 300, direction: Directions.Right, run: false},
+            {x1: 300, x2: 375, direction: Directions.Right, run: true}
+        ];
+        this.y = this.y1;
+        this.x = this.areas[2].x1 + 49;
+        this.id = null;
+    }
+
+    touchDown(pt, id){
+        this.id = id;
+        let state = this.getState(pt);
+        if (state)
+            this.x = pt.x;
+        return state;
+    }
+
+    touchMove(pt, id){
+        if (id == this.id){            
+            let state = this.getState(pt);
+            if (state){
+                this.x = pt.x;
+                return state;
+            }else if (pt.x > this.areas[4].x1){
+                this.x = this.areas[4].x1 + 49;
+                return {direction: Directions.Right, run: true};
+            }else if (pt.x < this.areas[0].x1){
+                this.x = this.areas[0].x1 + 49;
+                return {direction: Directions.Left, run: true};
+            }
+        }
+        return null;
+    }
+
+    touchUp(id){                
+        if (id == this.id){
+            this.id = null;
+            this.x = this.areas[2].x1 + 49;
+            return {direction: Directions.Idle, run: false};
+        }
+    }
+
+    getState(pt){        
+        if (pt.y >= this.y1 && pt.y <= this.y2){
+            for (let a of this.areas){
+                if (pt.x >= a.x1 && pt.x < a.x2 ){
+                    return {direction: a.direction, run: a.run};
+                }
+            }
+        }
+        return null;
+    }
+}
+
 class Touch{	
 	constructor(canvas){
 		this.direction = Directions.Idle;
@@ -13,6 +74,8 @@ class Touch{
         this.rightButton = {x:  130, y:440, w: 98, h: 98};
         this.jumpButton  = {x: 1040, y:440, w: 98, h: 98};
 
+        this.joystick = new Joystick();
+
 		canvas.addEventListener('touchstart', (e)=>{
             
             if (canvas.requestFullscreen && !canvas.fullscreenElement){
@@ -27,13 +90,22 @@ class Touch{
 
                 let pt = this.fullScreenToCanvas(canvas, x,y);
                 
+                let state = this.joystick.touchDown(pt, touchobj.identifier);
+                if (state){
+                    this.direction = state.direction;
+                    this.run = state.run;
+                }
+
+                /*
                 if (this.overlaps(pt, this.leftButton)){
                     this.direction = Directions.Left;
                     this.directionTouchIdentifier = touchobj.identifier;
+                    navigator.vibrate(100);
                 } else if (this.overlaps(pt, this.rightButton)){
                     this.direction = Directions.Right;
                     this.directionTouchIdentifier = touchobj.identifier;
-                } 
+                    navigator.vibrate(100);
+                } */
 
                 if (this.overlaps(pt, this.jumpButton)){
                     this.jump = true;
@@ -52,15 +124,24 @@ class Touch{
 
                 let pt = this.fullScreenToCanvas(canvas, x,y);
                 
-                if (this.directionTouchIdentifier == touchobj.identifier){
+                
+                    let state = this.joystick.touchMove(pt, touchobj.identifier);
+                    if (state){
+                        this.direction = state.direction;
+                        this.run = state.run;
+                    }
+
+                    //if (this.directionTouchIdentifier == touchobj.identifier){
+                    /*
                     if (this.overlaps(pt, this.leftButton)){
                         this.direction = Directions.Left;
                     }else if (this.overlaps(pt, this.rightButton)){
                         this.direction = Directions.Right;
-                    } else {
-                        this.direction = Directions.Idle;
                     }
-                }
+                    else {
+                        this.direction = Directions.Idle;
+                    }*/
+                //}
             }
 			e.preventDefault();
 		}, false);
@@ -71,6 +152,11 @@ class Touch{
                 let x = (touchobj.clientX - rect.left);
                 let y = (touchobj.clientY - rect.top);
                 
+                let state = this.joystick.touchUp(touchobj.identifier);
+                if (state){
+                    this.direction = state.direction;
+                    this.run = state.run;
+                }
                 //let pt = this.fullScreenToCanvas(canvas, x,y);
                 
                 if (this.directionTouchIdentifier == touchobj.identifier){
@@ -112,8 +198,16 @@ class Touch{
     }
 
     draw(gfx){
-		gfx.drawButton(this.leftButton.x,  this.leftButton.y, 0);
-		gfx.drawButton(this.rightButton.x, this.rightButton.y,1);
+        gfx.drawButton(this.joystick.x - 49,  this.joystick.y, 0);
+
+        gfx.drawButton(this.joystick.areas[0].x1,  this.joystick.y, 0);
+        gfx.drawButton(this.joystick.areas[1].x1,  this.joystick.y, 0);
+        gfx.drawButton(this.joystick.areas[2].x1,  this.joystick.y, 0);
+        gfx.drawButton(this.joystick.areas[3].x1,  this.joystick.y, 0);
+        gfx.drawButton(this.joystick.areas[4].x1,  this.joystick.y, 0);
+
+		//gfx.drawButton(this.leftButton.x,  this.leftButton.y, 0);
+		//gfx.drawButton(this.rightButton.x, this.rightButton.y,1);
 		gfx.drawButton(this.jumpButton.x,  this.jumpButton.y, 2);
     }
 }
